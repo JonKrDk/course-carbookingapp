24933266ba55df82121e09d3a4da4415c44e7eac
Jon Kristensen <jon@jonkr.dk>
2022-10-02 16:48:08 +0200

    Added a Cars Index Page

---
+++ CarBookingApp/Pages/Cars/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarBookingApp.Pages.Cars
+{
+    public class IndexModel : PageModel
+    {
+        public string Heading { get; set; }
+
+        public void OnGet()
+        {
+            Heading = "List of Cars";
+        }
+    }
+}
